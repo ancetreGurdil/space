513747f2ccb4df5a161fad32de3658cd5fbb4ce9
guillaume <guillaume.kerherve@scalian.com>
2023-04-19 22:04:59 +0200

    création des points de la petite ourse + univers

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,158 +1,144 @@
-import { animate } from '@angular/animations';
-import { Component,OnInit,AfterViewInit } from '@angular/core';
-import * as THREE from 'three';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import * as THREE from 'three'
+import { fromEvent, Observable, Subscription } from 'rxjs';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.scss']
 })
-export class TestComponent implements OnInit,AfterViewInit {
+export class TestComponent implements OnInit, AfterViewInit {
 
-  /* @ViewChild('canvas')
-  private canvasRef!: ElementRef
-  //proprieté du cube
-  @Input() public texture: string = '/assets/texture.jpg'
-  @Input() public size: number = 200;
-  @Input() public rotationSpeedY:number = 0.01
-  @Input() public rotationSpeedX:number = 0.05
-  //proprieté de la scene
-  @Input() public cameraZ: number = 400;
-  @Input() public fov: number = 1;
-  @Input('nearClipping') public nearClippingPlane = 1;
-  @Input('farClipping') public farClippingPlane = 1000;
+  public controls: OrbitControls;
+  public renderer: THREE.WebGLRenderer;
+  public camera: THREE.PerspectiveCamera;
+  public scene: THREE.Scene;
+  public resizeObservable: Observable<Event> = new Observable;
+  public resizeSubscription: Subscription = new Subscription;
 
-  private camera!: THREE.PerspectiveCamera;
-  private get canvas(): HTMLCanvasElement {
-    return this.canvasRef.nativeElement
-  }
-  private loader = new THREE.TextureLoader();
-  private geometry = new THREE.BoxGeometry(1,1,1);
-  private material = new THREE.MeshBasicMaterial({map: this.loader.load(this.texture)});
-  private cube: THREE.Mesh = new THREE.Mesh(this.geometry,this.material);
-  private renderer!: THREE.WebGL1Renderer;
-  private scene!: THREE.Scene ;
-
-
-
-  private createScene(){
-    //*Scene
+  constructor(){
     this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color(0x000000);
-    this.scene.add(this.cube);
-    //*Camera
-    let aspectRatio = this.getAspectRatio();
-    this.camera = new THREE.PerspectiveCamera(
-      this.fov,
-      aspectRatio,
-      this.nearClippingPlane,
-      this.farClippingPlane
-    )
-    this.camera.position.z = this.cameraZ;
+    this.camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 500 );
+    this.renderer = new THREE.WebGLRenderer();
+    this.controls = new OrbitControls( this.camera, this.renderer.domElement);
   }
-
-  private getAspectRatio(){
-    return this.canvas.clientWidth / this.canvas.clientHeight;
-  }
-
-  private cubeAnimation(){
-    this.cube.rotation.x += this.rotationSpeedX;
-    this.cube.rotation.y += this.rotationSpeedY;
-  }
-
-  private startRenderingLoop(){
-    //*Renderer
-    this.renderer = new THREE.WebGL1Renderer({canvas: this.canvas})
-    this.renderer.setPixelRatio(devicePixelRatio);
-    this.renderer.setSize(this.canvas.clientWidth,this.canvas.clientHeight);
-
-    let component = this;
-    (function render(){
-      requestAnimationFrame(render);
-      component.cubeAnimation();
-      component.renderer.render(component.scene,component.camera);
-    })
-  } */
-
-
-  private camera!: THREE.PerspectiveCamera;
-  private renderer!: THREE.WebGLRenderer;
-  private scene!: THREE.Scene ;
-  private controls!: OrbitControls;
 
   ngOnInit(): void {
-    this.scene = new THREE.Scene;
+    this.resizeObservable = fromEvent(window, 'resize')
+    this.resizeSubscription = this.resizeObservable.subscribe( evt => {
+      this.onWindowResize()
+    });
 
-    this.camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
-    this.camera.layers.enable(1);
-
-    this.renderer = new THREE.WebGLRenderer({antialias:true});
-    this.renderer.setSize(window.innerWidth,window.innerHeight);
-    this.renderer.setPixelRatio(window.devicePixelRatio);
-
-    this.controls = new OrbitControls( this.camera, this.renderer.domElement);
-    this.controls.enabled = true;
-    /* this.controls.enableZoom = true;
-    this.controls.enablePan = true;
-    this.controls.enableDamping = true;
-    this.controls.rotateSpeed = -0.25;
-    */
-    this.controls.listenToKeyEvents(window);
-    document.body.appendChild(this?.renderer?.domElement)
-
-
-    let materialArray: Array<any> = [];
-    let texture_ft = new THREE.TextureLoader().load('assets/1.png');
-    let texture_bk = new THREE.TextureLoader().load('assets/2.png');
-    let texture_up = new THREE.TextureLoader().load('assets/3.png');
-    let texture_dn = new THREE.TextureLoader().load('assets/4.png');
-    let texture_rt = new THREE.TextureLoader().load('assets/5.png');
-    let texture_lt = new THREE.TextureLoader().load('assets/6.png');
-
-
-    materialArray.push(new THREE.MeshBasicMaterial({map: texture_ft}))
-    materialArray.push(new THREE.MeshBasicMaterial({map: texture_bk}))
-    materialArray.push(new THREE.MeshBasicMaterial({map: texture_up}))
-    materialArray.push(new THREE.MeshBasicMaterial({map: texture_dn}))
-    materialArray.push(new THREE.MeshBasicMaterial({map: texture_rt}))
-    materialArray.push(new THREE.MeshBasicMaterial({map: texture_lt}))
-
-    for(let i=0;i<6;i++){
-      materialArray[i].side  = THREE.BackSide;
-    }
-
-
-    let skyBoxGeo = new THREE.BoxGeometry(100,100,100);
-    skyBoxGeo.scale(1,1,1);
-    let skybox = new THREE.Mesh(skyBoxGeo);
-    skybox.layers.enable(1);
-
-    this.camera.layers.enable(3);
-    let cube = new THREE.Mesh(new THREE.BoxGeometry(1,1,1), new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe : true}));
-    cube.position.x = 0.5;
-    cube.position.y = 0.5;
-    cube.position.z = -0.5;
-    cube.layers.set(3);
-    this.scene.add(cube);
-    this.scene.add(skybox);
-
-
-    this.renderer.render(this?.scene,this?.camera);
-    requestAnimationFrame(animate);
 
   }
 
+  ngAfterViewInit(): void {
+    this.addRenderer()
+    this.addControls();
+    this.addGeometry();
+    //
+    this.render();
+  }
 
-  render(){
+  addRenderer() {
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.xr.enabled = true;
+
+    document.body.appendChild(this.renderer.domElement);
+  }
+
+  addControls(){
+    this.controls.enabled = true;
+    this.controls.enableZoom = false;
+    this.camera.position.z = -0.01;
+    this.camera.position.y = 25 ;
+
+
+  }
+
+  addGeometry(){
+    let axesHelper = new THREE.AxesHelper( 5 ).setColors(new THREE.Color("rgb(255, 0, 0)"),new THREE.Color("rgb(0,255, 0)"),new THREE.Color("rgb(0, 0, 255)"));
+    this.scene.add( axesHelper );
+    this.camera.layers.enable(1);
+
+    const vertices = [];
+    for (let i = 0; i < 1000; i++) {
+      vertices.push(
+        new THREE.Vector3(
+          Math.random() * 600 - 300 + 100,
+          Math.random() * 600 - 300 + 100,
+          Math.random() * 600 - 300 + 100
+        )
+      );
+    }
+    const bufferGeometry = new THREE.BufferGeometry().setFromPoints(vertices);
+    const material = new THREE.PointsMaterial({ size: 1, color: 0xffffff });
+    const mesh = new THREE.Points(bufferGeometry, material);
+    this.scene.add(mesh);
+
+
+    /* const geometry = new THREE.BoxGeometry(400,400,400); */
+
+    /* geometry.scale(0.5, 0.5, -0.5) */
+
+   /*  const materials = [];
+
+    let texture_ft = new THREE.TextureLoader().load('assets/front.png')
+    let texture_bk = new THREE.TextureLoader().load('assets/back.png')
+    let texture_up = new THREE.TextureLoader().load('assets/up.png')
+    let texture_dn = new THREE.TextureLoader().load('assets/down.png')
+    let texture_rt = new THREE.TextureLoader().load('assets/right.png')
+    let texture_lt = new THREE.TextureLoader().load('assets/left.png')
+
+    materials.push(new THREE.MeshBasicMaterial({map: texture_ft}));
+    materials.push(new THREE.MeshBasicMaterial({map: texture_bk}));
+    materials.push(new THREE.MeshBasicMaterial({map: texture_up}));
+    materials.push(new THREE.MeshBasicMaterial({map: texture_dn}));
+    materials.push(new THREE.MeshBasicMaterial({map: texture_rt}));
+    materials.push(new THREE.MeshBasicMaterial({map: texture_lt})); */
+
+
+
+    /* const skyBox = new THREE.Mesh(geometry,new THREE.MeshBasicMaterial({color:'grey'}));
+    skyBox.layers.set(1);
+    this.scene.add(skyBox); */
+
+
+    let meshSpheresPtOursArray = [];
+    for(let i = 0;i<7;i++){
+      let meshSphere : THREE.Mesh = new THREE.Mesh(new THREE.SphereGeometry(1,32,16), new THREE.MeshBasicMaterial({color: 'rgb(0,255,0)'}));
+      meshSpheresPtOursArray.push(meshSphere)
+      this.scene.add(meshSphere);
+    }
+    // position de la petite ourse
+    meshSpheresPtOursArray[0].position.set(0,-150,300);
+    meshSpheresPtOursArray[1].position.set(10,-150,260);
+    meshSpheresPtOursArray[2].position.set(12,-150,210);
+    meshSpheresPtOursArray[3].position.set(-2,-150,170);
+    meshSpheresPtOursArray[4].position.set(-25,-150,140);
+    meshSpheresPtOursArray[5].position.set(14,-150,153);
+    meshSpheresPtOursArray[6].position.set(-15,-150,120);
+
+  }
+
+  render() {
     this.controls.update();
     this.renderer.render(this.scene, this.camera);
     this.renderer.setAnimationLoop(this.render.bind(this));
   }
 
-  ngAfterViewInit(): void {
-    this.render();
+  onWindowResize() {
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    this.renderer.setPixelRatio(window.devicePixelRatio)
+    this.camera.aspect = window.innerWidth / window.innerHeight
+    this.camera.updateProjectionMatrix()
   }
 
+  ngOnDestroy(){
+    this.resizeSubscription.unsubscribe()
+  }
+
+    //TODO: https://www.npmjs.com/package/buttonvr
 }
